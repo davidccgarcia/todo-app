aa341ac5cef7dd500f53077bcab2a41cb38839fa
David García <ccristhiangarcia@gmail.com>
2024-01-03 09:21:37 -0500

    Validaciones y optimizaciones

--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from '../models/todo.model';
 import { createTodoHTML } from './create-todo-html';
 
+let element;
+
 /**
  * Renderiza listado de Todos
  * @param {String} elementId 
@@ -8,8 +10,11 @@ import { createTodoHTML } from './create-todo-html';
  */
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    // TODO: referencia
-    const element = document.querySelector( elementId );
+    if ( !element ) element = document.querySelector( elementId );
+
+    if ( !element ) throw new Error(`Element ${ elementId } not found.`);
+
+    element.innerHTML = '';
 
     todos.forEach( todo => {
         element.append( createTodoHTML(todo) );
