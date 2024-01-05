34f17860afa19df879ade00c621de6c659b4b95f
David García <ccristhiangarcia@gmail.com>
2024-01-05 08:29:16 -0500

    (Practice): localStorage

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -18,12 +18,20 @@ const state = {
 }
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
     console.log('InitStore 🥑');
 }
 
 const loadStore = () => {
-    throw new Error('Not implemented');
+    if ( !localStorage.getItem('state') ) return;
+
+    const { todos = [], filter = Filters.All } = JSON.parse( localStorage.getItem('state') );
+    state.todos = todos;
+    state.filter = filter;
+}
+
+const saveStateToLocalStorage = () => {
+    localStorage.setItem('state', JSON.stringify(state));
 }
 
 const getTodos = ( filter = Filters.All ) => {
@@ -47,6 +55,7 @@ const addTodo = ( description ) => {
     if ( !description ) throw new Error('Description is required.');
 
     state.todos.push( new Todo(description) );
+    saveStateToLocalStorage();
 }
 
 /**
@@ -63,6 +72,7 @@ const toggleTodo = ( todoId ) => {
             todo.done = !todo.done
         }
 
+        saveStateToLocalStorage();
         return todo;
 
     } );
@@ -76,6 +86,7 @@ const deleteTodo = ( todoId ) => {
     if ( !todoId ) throw new Error('todoId is required.');
 
     state.todos = state.todos.filter( todo => todo.id !== todoId );
+    saveStateToLocalStorage();
 }
 
 const deleteCompleted = () => {
